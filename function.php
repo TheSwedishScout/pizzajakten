54856b788e7ba647267d7f0209cb0def70c6cbc1
Max Timje <max@timje.se>
2017-09-22 11:07:32 +0200

    Init
    Signed-off-by: Max Timje <max@timje.se>

---
+++ function.php
@@ -0,0 +1,38 @@
+<?php 
+function test_input($data) {
+
+	$data = trim($data);
+
+	$data = stripslashes($data);
+
+	$data = htmlspecialchars($data);
+
+	$conn = connect_to_db();
+
+	$data = mysqli_real_escape_string ( $conn , $data );
+
+	return $data;
+	$conn->close();
+
+}
+
+function connect_to_db(){
+	include_once("config.php");
+	$conn = mysqli_connect(HOST, USER, PASSWORD, DATABASE) or die(mysqli_error());
+
+	if ($conn->connect_error) {
+
+		die("Connection failed: " . $conn->connect_error);
+
+	} 
+
+	$sql_main = "SET NAMES 'utf8'";
+	$result = $conn->query($sql_main);
+	//mysqli::set_charset('utf8'); // when using mysqli
+
+	$sql_second ="CHARSET 'utf8'";
+	$result = $conn->query($sql_second);
+	//sets utf-8 as CHARSET in mySQL§
+	return $conn;
+}
+?>
