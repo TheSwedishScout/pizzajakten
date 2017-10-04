3c5b916402be3318d305546425761e2914364d7b
Max Timje <ttiimmjjee@hotmail.com>
2017-10-04 12:35:59 +0200

    Db setting updated was using the book db before

--- config.php
+++ config.php
@@ -5,5 +5,5 @@
 define("HOST", "localhost");     // The host you want to connect to.
 define("USER", "root");    // The database username. 
 define("PASSWORD", "");    // The database password. 
-define("DATABASE", "book-club");    // The database name.
+define("DATABASE", "pizza");    // The database name.
 ?>
