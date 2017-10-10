5bc5d5e903cdb8dc7d2f1586ea0b9a3716636242
Max Timje <max@timje.se>
2017-10-10 18:01:30 +0200

    INSERT PIZZOR FUNKAR!!! med de ingredienser som finns i db

--- assets/gettopalikeingredients.php
+++ assets/gettopalikeingredients.php
@@ -8,23 +8,11 @@ if(isset($_GET['term'])){
 	$conn = connect_to_db();
 	$return = [];
 
-	$result = $conn->query("SELECT namn  FROM `ingredienser` WHERE `namn` LIKE '%{$term}%' LIMIT 5");
+	$result = $conn->query("SELECT namn FROM `ingredienser` WHERE `namn` LIKE '%{$term}%' LIMIT 5");
 	
 	//$result = $result->fetch_all(MYSQLI_ASSOC);
 	$return[] =  $result->fetch_all(MYSQLI_ASSOC);
 	echo json_encode($return, JSON_UNESCAPED_UNICODE);
 
 }
-
-
-
-
-
-
-
-
-
-
-
-
 ?>
