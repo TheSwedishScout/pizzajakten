5ad0a5128df790b1b0696eaf03dd94a2a9ce2f95
Max Timje <max@timje.se>
2017-10-10 12:22:05 +0200

    remove strange characters tings

--- config.php
+++
@@ -1,9 +0,0 @@
-<?php
-/**
- * These are the database login details
- */  
-define("HOST", "localhost");     // The host you want to connect to.
-define("USER", "root");    // The database username. 
-define("PASSWORD", "root");    // The database password. 
-define("DATABASE", "pizza");    // The database name.
-?>
