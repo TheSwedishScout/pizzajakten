bc90d36ed195217a7a944df7841ee0d6b813fd15
Max Timje <max@timje.se>
2017-10-10 14:02:20 +0200

    continuation on input and the grid is updated

--- admin/js/input.js
+++ admin/js/input.js
@@ -24,11 +24,42 @@ document.addEventListener('DOMContentLoaded', function(e) {
 				
 			}
 	}
+	/*PIZZA*/
+
+	var pizza= {};
 	document.getElementById('ingredinesIn').addEventListener("input", function (e) {
 		//console.log(this.value.length);
 		if(this.value.length >= 3){
-			debugger;
+			let input = this;
+			ajax.get("../assets/gettopalikeingredients.php", {'term':this.value}, function (data) {
+				var options= JSON.parse(data)[0];
+
+				input.value = options[0].namn;
+				
+			})
 		}
 	})
-	
+	pizza.ingredienser=[];
+	document.getElementById('ingredinesIn').addEventListener("keydown", function (e) {
+		if(e.key == "Tab") {
+            
+            if(e.preventDefault) {
+                e.preventDefault();
+            }
+            /*add to list of ingrediens*/
+            var ul = document.getElementById('list');
+            var li = document.createElement("li");
+            li.innerText = this.value;
+            ul.appendChild(li);
+            ajax.get("../assets/gettopalikeingredients.php", {'term':this.value}, function (data) {
+				var options= JSON.parse(data)[0];
+
+				pizza.ingredienser.push(options[0].namn);
+				console.log(pizza);
+			})
+        }
+	})
+	document.getElementById('ingredinesIn').addEventListener("submit", function(){
+		
+	})
 })
