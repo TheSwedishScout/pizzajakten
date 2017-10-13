73ef95837a04bd43a42b240be513ec55809dee24
Max Timje <max@timje.se>
2017-10-13 12:04:43 +0200

    get categories tabs fron db

--- index.php
+++ index.php
@@ -4,25 +4,23 @@
 		$tab = test_input($_GET['t']);
 	}
 	//$conn = connect_to_db();
-	$conn = connect_to_db();
-				//$sql = "SELECT id, first_name, last_name FROM author";
-				/*$sql = "SELECT category FROM `ingredienser` GROUP BY category";
-				if ($result = $conn->query($sql)) {
-					while ($row = $result->fetch_assoc()) {
-						echo("<option value='".$row['id']."' >". $row['first_name']." ".$row['last_name']."</option>");
-				    }
-				}
-				$conn->close();*/
 ?>
 
 <!-- Filkarna -->
-<div class="tabs">
-	<a class="shadow <?php echo !isset($tab) || $tab == 'GRÖNSAKER' ? 'active' : null; ?>" href="?t=GRÖNSAKER">GRÖNSAKER</a>
-	<a class="shadow <?php echo isset($tab) && $tab == 'TOPPING' ? 'active' : null; ?>" href="?t=TOPPING">TOPPING</a>
-	<a class="shadow <?php echo isset($tab) && $tab == 'KÖTT' ? 'active' : null; ?>" href="?t=KÖTT">KÖTT</a>
-	<a class="shadow <?php echo isset($tab) && $tab == 'OSTER' ? 'active' : null; ?>" href="?t=OSTER">OSTER</a>
-	<a class="shadow <?php echo isset($tab) && $tab == 'SÅSER' ? 'active' : null; ?>" href="?t=SÅSER">SÅSER</a>
-</div>
+<ul class="tabs">
+	<?php
+		$conn = connect_to_db();
+		//$sql = "SELECT id, first_name, last_name FROM author";
+		$sql = "SELECT category FROM `ingredienser` GROUP BY category";
+		if ($result = $conn->query($sql)) {
+			while ($row = $result->fetch_assoc()) {
+				echo("<li class='shadow' >". $row['category']."</li>");
+		    }
+		}
+		$conn->close();
+
+	?>
+</ul>
 
 <!-- Main sidorna-->
 <main class="left">
