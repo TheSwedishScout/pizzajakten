8d7dc96f3bbc7bec3cce450856ab37942cab2374
Miranda Nordholm <mirnor890@hotmail.com>
2017-10-15 16:29:35 +0200

    Knappar på pizzerior

--- pizzerior.php
+++ pizzerior.php
@@ -14,26 +14,42 @@
 </main>
 <main class="right pizzerior">
 	<h2></h2>
-        <!--Karta på pizzerior med knappar för location, rating och price-->
+        
         <ul>
         	<li>
-        		<h3>Margahreta</h3>
-        		<h3>Biblos</h3>
-        		<p>Drottninggatan 18, 561 31 Huskvarna</p>
-        		<p>85kr</p>
-        	</li>
-        	<li>
-        		<h3>Margahreta</h3>
-        		<h3>Biblos</h3>
-        		<p>Drottninggatan 18, 561 31 Huskvarna</p>
-        		<p>85kr</p>
-        	</li>
-        	<li>
-        		<h3>Margahreta</h3>
-        		<h3>Biblos</h3>
-        		<p>Drottninggatan 18, 561 31 Huskvarna</p>
-        		<p>85kr</p>
-        	</li>
+
+                <h3>Margahreta</h3>
+                <h3>Biblos</h3>
+                <p>Drottninggatan 18, 561 31 Huskvarna</p>
+                <p>85kr</p>
+                
+                <form action="">
+                    <input type="submit" name="Välj denna" value="Välj pizza">
+                    <input type="hidden" name="">
+                </form>
+            </li>
+            <li>
+                <h3>Margahreta</h3>
+                <h3>Biblos</h3>
+                <p>Drottninggatan 18, 561 31 Huskvarna</p>
+                <p>85kr</p>
+                
+                <form action="">
+                    <input type="submit" name="Välj denna" value="Välj pizza">
+                    <input type="hidden" name="">
+                </form>
+            </li>
+            <li>
+                <h3>Margahreta</h3>
+                <h3>Biblos</h3>
+                <p>Drottninggatan 18, 561 31 Huskvarna</p>
+                <p>85kr</p>
+                
+                <form action="">
+                    <input type="submit" name="Välj denna" value="Välj pizza">
+                    <input type="hidden" name="">
+                </form>
+            </li>
         </ul>
 </main>
 
