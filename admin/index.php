29458a284e289fb58fa86e09904a35fafa69eaa8
Miranda Nordholm <mirnor890@hotmail.com>
2017-10-26 15:48:49 +0200

    admin-panel textändringar

--- admin/index.php
+++ admin/index.php
@@ -39,10 +39,13 @@
 	</div>
 
     <ul class="admin">
-        <li><h2>Välkommen till adminpanelen! <br><br>Vill du...</h2></li>
-        <li><a href="input.php"><button><p>Uppdatera din pizzeria</p></a></button></li>
-        <li><h2>eller..</h2></li>
+        <li><h2>Välkommen till adminpanelen! <br><br>Vill du...</h2></li> 
         <li><a href="update.php"><button><p>Uppdatera en pizza</p></a></button></li>
+        <li><h2>eller..</h2></li>      
+        <li><a href="input.php"><button><p>Lägg till en ny pizza</p></a></button></li>
+        <li><h2>eller..</h2></li>
+        <li><a href="input.php"><button><p>Lägg till en pizzeria</p></a></button></li>
+        
     </ul>
     <?php
 	include '../footer.php';
