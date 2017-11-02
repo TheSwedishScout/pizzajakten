71a47d7b7a985734e5ae5ac95c6df4f2082ed594
Max Timje <max@timje.se>
2017-11-02 15:34:29 +0100

    updatera pizzor

--- admin/assets/updatepizza.php
+++ admin/assets/updatepizza.php
@@ -8,10 +8,7 @@ if(isset($_POST['pizza'])){
 	if (isset($_POST['pris'])) {
 		$pris = (int)test_input($_POST['pris']);
 	}
-	if (isset($_POST['ingredienser'])) {
-		$ingredienser = test_input($_POST['ingredienser']);
-		//var_dump($ingredienser);
-	}
+
 	$conn = connect_to_db();
 	$sql = "UPDATE pizzorinpizzeria SET name = ?, pris = ? WHERE id = ?";
 	$stmt = $conn->prepare($sql);
@@ -24,14 +21,32 @@ if(isset($_POST['pizza'])){
 	$response['sucsess'] = $stmt->affected_rows;
 
 	$result = $stmt->get_result();
+	$stmt->close();
 
 	//var_dump($stmt->get_result());
 
 	/*---------------------------------------Update ingredients---------------------------------------*/
 
+	//ta bort alla ingredienser och lägg till de i listan på nytt.
+	if (isset($_POST['ingredienser'])) {
+		$ingredienser = explode(',',($_POST['ingredienser']));
+	
+	//tar bort ingredienser från pizzan
+		$sql2 = "DELETE FROM ingredienseronpizza WHERE pizza = ?";
+		$stmt = $conn->prepare($sql2);
+		$stmt->bind_param("i", $id);
+		$stmt->execute();
+		$stmt->close();
+		$sql3 = "INSERT INTO ingredienseronpizza(ingrediens, pizza) VALUES (?,?)";
+		//var_dump($ingredienser);
+		foreach ($ingredienser as $ingrediens) {
+			$stmt = $conn->prepare($sql3);
+			$stmt->bind_param("si", $ingrediens, $id);
+			$stmt->execute();
+			$stmt->close();
+		}
+	}
 
-
-	$stmt->close();
 	$json = json_encode($response, JSON_UNESCAPED_UNICODE);
 	print_r($json);
 }
--- admin/js/pizzasinPizzeria.js
+++ admin/js/pizzasinPizzeria.js
@@ -48,28 +48,31 @@ document.addEventListener("DOMContentLoaded", function() {
 					/*ingrediener*/
 					thisp.ingredienser;
 					var removedIngred = [];
-					for(var ing2 of thisp.ingredienser){
-						(function () {
-							// body...
-							var ingredisd= ing2
+					function updateIngList(ingredienser) {
+						uling2.innerHTML ="";
+						for(var ing2 of ingredienser){
+							(function () {
+								// body...
+								var ingredisd= ing2
 
-							var liing2 = document.createElement("li");
-							var remove = document.createElement("a");
-							remove.innerText =" X";
-							remove.href="#";
-							remove.addEventListener('click',function(e) {
-								e.preventDefault();
-								removedIngred.push(ingredisd);
-								debugger;
-								this.parentNode.parentNode.removeChild(this.parentNode);
-							})
+								var liing2 = document.createElement("li");
+								var remove = document.createElement("a");
+								remove.innerText =" X";
+								remove.href="#";
+								remove.addEventListener('click',function(e) {
+									e.preventDefault();
+									removedIngred.push(ingredisd);
+									
+									this.parentNode.parentNode.removeChild(this.parentNode);
+								})
 
-							liing2.innerText = ing2;
-							liing2.appendChild(remove);
-							uling2.appendChild(liing2);
-						})()
+								liing2.innerText = ing2;
+								liing2.appendChild(remove);
+								uling2.appendChild(liing2);
+							})()
+						}
 					}
-					
+					updateIngList(thisp.ingredienser);
 					form.appendChild(uling2);
 					var display = document.createElement('ul');
 					form.appendChild(display);
@@ -77,8 +80,25 @@ document.addEventListener("DOMContentLoaded", function() {
 					var nying = document.createElement('input');
 					nying.type = 'text';
 					nying.placehoder = "Ny ingrediens";
-					form.appendChild(nying);
-					pizza.ingredienser=[];
+					//Eventlistener för ingredienser
+					/*---------------------------------------------------------------------------------------------------------------------------*/
+					/*---------------------------------------------------------------------------------------------------------------------------*/
+					/*---------------------------------------------------------------------------------------------------------------------------*/
+					nying.addEventListener("input", function (e) {
+						//console.log(this.value.length);
+
+						if(this.value.length >= 3 && e.inputType != "deleteContentBackward" && e.inputType != 'deleteWordBackward'){
+							let input = this;
+							ajax.get("../assets/gettopalikeingredients.php", {'term':this.value}, function (data) {
+								var options= JSON.parse(data)[0];
+								
+								if(options.length > 0){
+									input.value = options[0].namn;
+								}
+								
+							})
+						}
+					});
 					nying.addEventListener("keydown", function (e) {
 						
 						if(e.key == "Tab" || e.key == 'Enter') {
@@ -87,23 +107,26 @@ document.addEventListener("DOMContentLoaded", function() {
 				                e.preventDefault();
 				            }
 				            /*add to list of ingrediens*/
-				            var ul = display;
+
 				            var li = document.createElement("li");
 				            let input = this;
 				            ajax.get("../assets/gettopalikeingredients.php", {'term':this.value}, function (data) {
 								
 								var options= JSON.parse(data)[0];
-								if(typeof(options[0].namn) == "string")
-								pizza.ingredienser.push(options[0].namn);
-								
-				            	li.innerText = options[0].namn;
-								console.log(pizza);
-				            	ul.appendChild(li);
-				            	
+								if(typeof(options[0].namn) == "string"){
+									thisp.ingredienser.push(options[0].namn);
+									
+									updateIngList(thisp.ingredienser);
+								}
 				            	input.value = "";
 							})
 				        }
 					})
+					/*-------------------------------------------------------------------------------------------------*/
+					/*-------------------------------------------------------------------------------------------------*/
+					/*-------------------------------------------------------------------------------------------------*/
+					/*-------------------------------------------------------------------------------------------------*/
+					form.appendChild(nying);
 					/*knappar*/
 					/*avbryt spara*/
 					var avbryt = document.createElement('input');
@@ -133,7 +156,7 @@ document.addEventListener("DOMContentLoaded", function() {
 							}
 						}
 						var data = {'pizza':thisp.id, 'namn': thisp.name, 'pris':thisp.pris, 'ingredienser':thisp.ingredienser};
-						debugger;
+						console.log(data);
 						ajax.post("assets/updatepizza.php", data, reprint)
 						
 						li.lastChild.addEventListener('click', andra)
@@ -147,6 +170,7 @@ document.addEventListener("DOMContentLoaded", function() {
 				function reprint(data) {
 					// body...
 					debugger;
+					
 				}
 			}
 			)()
